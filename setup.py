d79c57ebc828223d59926b8c9bd59651abc4b969
Johan Lorenzo <gitlab@johan.lrnz.fr>
2021-10-24 14:19:54 +0200

    Port jobgraph to Python 3.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements/base.in", "r") as fp:
     requirements = fp.read().splitlines()
 
 setup(
-    name="gitlabci-jobraph",
+    name="gitlabci-jobgraph",
     version="1.0.0",
     description="Build Gitlab CI jobgraph",
     url="TODO",
@@ -12,10 +12,7 @@ setup(
     package_dir={"": "src"},
     install_requires=requirements,
     classifiers=(
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ),
     entry_points={"console_scripts": ["jobgraph = jobgraph.main:main"]},
     package_data={
