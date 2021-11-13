b61af4600f7e94ac6edf77d98beae734668db9e3
Johan Lorenzo <gitlab@johan.lrnz.fr>
2021-11-13 15:06:48 +0100

    Add gitlab.com as a known host and test connection before attempting any update

--- gitlab-ci/scripts/update-dependencies.sh
+++ gitlab-ci/scripts/update-dependencies.sh
@@ -7,17 +7,20 @@ set -ex
 GIT_REMOTE="${1:-origin}"
 GIT_BRANCH="${2:-update-jobgraph-dependencies}"
 
+GIT_REMOTE_URL="$(git remote get-url "$GIT_REMOTE")"
+URL_WITHOUT_HTTPS_NOR_BASIC_AUTH="$(echo "$GIT_REMOTE_URL" | sed 's/^https:\/\/\([^@]*@\)\?/git@/')"
+URL_WITH_COLON="${URL_WITHOUT_HTTPS_NOR_BASIC_AUTH/\//:}"
+SSH_URL="${URL_WITH_COLON%/}"
+SSH_TEST_URL="$(echo "$SSH_URL" | cut -d':' -f1)"
+
+ssh -T "$SSH_TEST_URL"
+
 jobgraph update-dependencies
 
 git switch --force-create "$GIT_BRANCH"
 git config user.name '[Bot] Jobgraph Cron'
 git config user.email ''
 git commit --all --message 'Run jobgraph update-dependencies' || (echo 'No updates found' && exit 0)
-
-GIT_REMOTE_URL="$(git remote get-url "$GIT_REMOTE")"
-URL_WITHOUT_HTTPS_NOR_BASIC_AUTH="$(echo "$GIT_REMOTE_URL" | sed 's/^https:\/\/\([^@]*@\)\?/git@/')"
-URL_WITH_COLON="${URL_WITHOUT_HTTPS_NOR_BASIC_AUTH/\//:}"
-SSH_URL="${URL_WITH_COLON%/}"
 
 git remote set-url --push "$GIT_REMOTE" "$SSH_URL"
 GIT_MAIN_BRANCH="$(git ls-remote --symref "$GIT_REMOTE" HEAD | awk '/^ref:/ {sub(/refs\/heads\//, "", $2); print $2}')"
