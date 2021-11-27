c6930c917604bfaeac91d240cd499826df9244ab
Johan Lorenzo <gitlab@johan.lrnz.fr>
2021-11-27 10:17:32 +0100

    Rename `reinstall-jobgraph` into `reinstall_jobgraph`

--- src/jobgraph/transforms/reinstall_jobgraph.py
+++ src/jobgraph/transforms/reinstall_jobgraph.py
@@ -6,7 +6,7 @@ transforms = TransformSequence()
 @transforms.add
 def add_before_script(config, jobs):
     for job in jobs:
-        if job.pop("reinstall-jobgraph", False):
+        if job.pop("reinstall_jobgraph", False):
             before_script = job.setdefault("before_script", [])
             before_script.append(
                 "pip install --prefix '/runner/.local' --no-deps --editable \"$CI_PROJECT_DIR\""
