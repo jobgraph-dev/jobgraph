c10ea6115eba6c4c0585e2aff8debd97c955ea26
Johan Lorenzo <gitlab@johan.lrnz.fr>
2022-01-05 07:40:00 +0100

    Move fetch_image_digest_from_gitlab_com() to util.docker_registries.gitlab

--- src/jobgraph/util/docker_registries/gitlab.py
+++ src/jobgraph/util/docker_registries/gitlab.py
@@ -1,4 +1,26 @@
-from jobgraph.util.docker_registries import build_image_full_location
+from jobgraph.util.docker_registries import (
+    build_image_full_location,
+    get_container_registry_session,
+    get_image_digest,
+    register_docker_registry_domain,
+)
+
+
+@register_docker_registry_domain("registry.gitlab.com")
+def fetch_image_digest_from_gitlab_com(image_data):
+    session = get_container_registry_session(
+        image_data,
+        {
+            "endpoint": "https://gitlab.com/jwt/auth",
+            "params": {
+                "client_id": "docker",
+                "offline_token": "true",
+                "service": "container_registry",
+            },
+            "auth_env_variables": ("CI_REGISTRY_USER", "CI_REGISTRY_PASSWORD"),
+        },
+    )
+    return get_image_digest(image_data, session)
 
 
 def get_image_full_location(
